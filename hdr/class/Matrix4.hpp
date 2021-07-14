d2f6510851bdb6ebc624ca3d0787f76f964c2bde
jmoucach <jpmoucachen@gmail.com>
2021-07-14 03:05:11 +0200

    Matrix: fix parametre dupliqué

--- hdr/class/Matrix4.hpp
+++ hdr/class/Matrix4.hpp
@@ -12,9 +12,9 @@ public:
 	float   matrix[16];
 	Matrix4(void);
 	~Matrix4(void);
-	void	translate(float x, float y, float y);
+	void	translate(float x, float y, float z);
 	void	translate(float xyz);
-	void	scale(float x, float y, float y);
+	void	scale(float x, float y, float z);
 	void	scale(float xyz);
 	void	rotate(float angle, e_axis axis);
 
