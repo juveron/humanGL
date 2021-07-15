970b384424a3f712995e1b9975844a49929fb508
jmoucach <jpmoucachen@gmail.com>
2021-07-15 17:26:49 +0200

    Ajout triangle test

--- src/init/main.cpp
+++ src/init/main.cpp
@@ -32,10 +32,10 @@ int main(void)
 
 
 		float vertices[] = {
-			   0.5f,  0.5f, 0.0f,  // top right
-			   0.5f, -0.5f, 0.0f,  // bottom right
-			  -0.5f, -0.5f, 0.0f,  // bottom left
-			  -0.5f,  0.5f, 0.0f   // top left 
+			0.5f,  0.5f, 0.0f,  // top right
+			0.5f, -0.5f, 0.0f,  // bottom right
+			-0.5f, -0.5f, 0.0f,  // bottom left
+			-0.5f,  0.5f, 0.0f   // top left 
 		};
 		unsigned int indices[] = {  // note that we start from 0!
 			0, 1, 3,  // first Triangle
@@ -66,23 +66,33 @@ int main(void)
 		// VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
 		glBindVertexArray(0);
 
-        glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+		glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
-        while (!glfwWindowShouldClose(window))
+		float deltaTime = 0.0f;
+		float lastFrame = 0.0f;
+		float angle = 0;
+
+		while (!glfwWindowShouldClose(window))
 		{
+			float currentTime = glfwGetTime();
+			deltaTime = currentTime - lastFrame;
+			lastFrame = currentTime;
 			processInput(window);
+
 			glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 			shader.use();
 			glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
-		   //glDrawArrays(GL_TRIANGLES, 0, 6);
-//		    shader.setMatrix("matrix", Matrix4::newIdentityMatrix().matrix);
-//			glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, 0);
+			//glDrawArrays(GL_TRIANGLES, 0, 6);
+			shader.setMatrix("matrix", Matrix4::newIdentityMatrix().matrix);
+			glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, 0);
 			Matrix4 matrix1;
-//            matrix1.scale(0.5f);
-//            matrix1.translate(0.05f, 0.0f, 0.0f);
-            matrix1.rotate(90, X_AXIS);
-            shader.setMatrix("matrix", matrix1.matrix);
-            glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, 0);
+			matrix1.scale(0.5f);
+			matrix1.translate(0.05f, 0.0f, 0.0f);
+			angle += 45 * deltaTime;
+			matrix1.rotate(angle, Y_AXIS);
+			shader.setMatrix("matrix", matrix1.matrix);
+			glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, 0);
 			glfwSwapBuffers(window);
 			glfwPollEvents();
 		}
