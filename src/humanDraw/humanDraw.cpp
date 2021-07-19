7e7f02c239246c07302c5059e907275682134dae
jmoucach <jpmoucachen@gmail.com>
2021-07-19 15:31:29 +0200

    humanDraw: Fix ordre push et pop

--- src/humanDraw/humanDraw.cpp
+++ src/humanDraw/humanDraw.cpp
@@ -10,19 +10,19 @@ void	drawLimb(Limb *limb, MatrixStack &matrixStack, Shader &shader)
 	std::vector<Limb *>::iterator iter = limb->children.begin();
 	std::vector<Limb *>::iterator iterEnd = limb->children.end();
 
-	matrixStack.topMatrix().multiply(limb->currentMat);
+	Matrix4 top = matrixStack.topMatrix();
+
+	matrixStack.pushMatrix(limb->currentMat);
+	matrixStack.topMatrix().multiply(top);
+
 	matrix4 = limb->scaleMat * matrixStack.topMatrix();
-	// std::cout << "Top matrix:" << std::endl;
-	// matrixStack.topMatrix().print();
-	// std::cout << "===================" << std::endl;
+
 	shader.setMatrix("matrix", matrix4.matrix);
 	glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, 0);
 	while (iter != iterEnd)
 	{
-		matrixStack.pushMatrix();
-		// std::cout << "child" << std::endl;
 		drawLimb(*iter, matrixStack, shader);
-		matrixStack.popMatrix();
 		iter++;
 	}
+	matrixStack.popMatrix();
 }
