eb3eb0c6e2410cebeb628317d932967e05007cb5
jmoucach <jpmoucachen@gmail.com>
2021-07-20 16:12:12 +0200

    Limb: Update limb pour avoir des fct rotate, translate et scale propre a elle

--- class/Limb.cpp
+++ class/Limb.cpp
@@ -13,6 +13,37 @@ Limb::~Limb(void)
 {
 }
 
+void Limb::rotateLimb(float const angle, e_axis const axis)
+{
+	this->currentMat.rotate(angle, axis);
+}
+
+void Limb::translateLimb(float const x, float const y, float const z)
+{
+	this->currentMat.translate(x, y, z);
+}
+
+void Limb::translateLimb(float const xyz)
+{
+	this->currentMat.translate(xyz);
+}
+
+void Limb::scaleLimb(float const x, float const y, float const z)
+{
+	this->scale.x = x;
+	this->scale.y = y;
+	this->scale.z = z;
+	this->scaleMat.scale(x, y, z);
+}
+
+void Limb::scaleLimb(float const xyz)
+{
+	this->scale.x = xyz;
+	this->scale.y = xyz;
+	this->scale.z = xyz;
+	this->scaleMat.scale(xyz);
+}
+
 void Limb::addChild(Limb *child)
 {
 	this->children.push_back(child);
--- hdr/class/Limb.hpp
+++ hdr/class/Limb.hpp
@@ -12,12 +12,19 @@ public:
 	Limb(Limb const &c) = delete;
 	~Limb(void);
 
+	Vector3f			scale;
 	Matrix4				scaleMat;
 	Matrix4				currentMat;
 	Limb				*parent;
 	std::vector<Limb *>	children;
 
-	void	addChild(Limb *child);
+	void	rotateLimb(float const angle, e_axis const axis);
+	void	translateLimb(float const x, float const y, float const z);
+	void	translateLimb(float const xyz);
+	void	scaleLimb(float const x, float const y, float const z);
+	void	scaleLimb(float const xyz);
+
+	void	addChild(Limb * child);
 	void	removeChild(Limb *child);
 
 	void	setParent(Limb *parent);
