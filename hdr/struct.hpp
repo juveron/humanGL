366ece21ad117d740d09741cc88ff1f2b132e929
jmoucach <jpmoucachen@gmail.com>
2021-07-22 16:26:03 +0200

    struct: Ajout struct body

--- hdr/struct.hpp
+++ hdr/struct.hpp
@@ -3,10 +3,17 @@
 
 #include "./HumanGL.hpp"
 
-typedef enum axis {
+class Limb;
+
+typedef enum				axis {
 	X_AXIS,
 	Y_AXIS,
 	Z_AXIS
-}            e_axis;
+}							e_axis;
+
+typedef struct				body {
+	Limb					*limb;
+	std::vector<Limb *>		limbs;
+}							s_body;
 
 #endif
