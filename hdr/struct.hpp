bdeee8b304dfe1461a730723fc4e37ced4d1e086
jmoucach <jpmoucachen@gmail.com>
2021-07-26 17:28:16 +0200

    Ajout enum humanBody

--- hdr/struct.hpp
+++ hdr/struct.hpp
@@ -11,6 +11,23 @@ typedef enum				axis {
 	Z_AXIS
 }							e_axis;
 
+typedef enum	humanBody {
+	TORSO,
+	HEAD,
+	RIGHT_ARM,
+	RIGHT_FOREARM,
+	RIGHT_HAND,
+	LEFT_ARM,
+	LEFT_FOREARM,
+	LEFT_HAND,
+	RIGHT_THIGH,
+	RIGHT_LEG,
+	RIGHT_FOOT,
+	LEFT_THIGH,
+	LEFT_LEG,
+	LEFT_FOOT
+}				e_humanBody;
+
 typedef struct				body {
 	int						selectedLimb;
 	Limb					*limb;
