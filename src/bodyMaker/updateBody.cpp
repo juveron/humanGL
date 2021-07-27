aedfc41c411f212d7302277fb038e0a371068c5a
jmoucach <jpmoucachen@gmail.com>
2021-07-27 16:20:07 +0200

    rework updateBody pour nouvelles matrices limb

--- src/bodyMaker/updateBody.cpp
+++ src/bodyMaker/updateBody.cpp
@@ -1,16 +1,20 @@
 #include "HumanGL.hpp"
 
-void updateBody(std::vector<Limb*> &limbs, const int size, std::vector<std::array<Matrix4, 2>> anim)
+void updateBody(std::vector<Limb*> &limbs, const int size, std::vector<std::array<Matrix4, 3>> anim)
 {
 	int i = 0;
 
 	while (i < size)
 	{
-		limbs[i]->currentMat = anim[i][0];
-		limbs[i]->scaleMat = anim[i][1];
-		limbs[i]->scale.x = anim[i][1].matrix[0];
-		limbs[i]->scale.y = anim[i][1].matrix[5];
-		limbs[i]->scale.z = anim[i][1].matrix[10];
+		limbs[i]->rotateMat = anim[i][0];
+		limbs[i]->translateMat = anim[i][1];
+		limbs[i]->translation.x = anim[i][1].matrix[12];
+		limbs[i]->translation.y = anim[i][1].matrix[14];
+		limbs[i]->translation.z = anim[i][1].matrix[13];
+		limbs[i]->scaleMat = anim[i][2];
+		limbs[i]->scale.x = anim[i][2].matrix[0];
+		limbs[i]->scale.y = anim[i][2].matrix[5];
+		limbs[i]->scale.z = anim[i][2].matrix[10];
 		i++;
 	}
 }
