f943bb25527a52f2727ac225cb87c4ec44723a54
jmoucach <jpmoucachen@gmail.com>
2021-08-26 12:47:18 +0200

    shader ajout texture

--- src/shader/fragmentShader.glsl
+++ src/shader/fragmentShader.glsl
@@ -1,10 +1,13 @@
 #version 410 core
 
+in vec2 TexCoord;
+
 out vec4 FragColor;
 
 uniform vec3 color;
+uniform sampler2D ourTexture;
 
 void main()
 {
-	FragColor = vec4(color, 1.0f);
+	FragColor = texture(ourTexture, TexCoord) * vec4(color, 1.0f);
 }
