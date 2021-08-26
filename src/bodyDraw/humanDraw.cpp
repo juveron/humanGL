4d7b11f0a01ad4d13ad3dc7da13e92459a398672
jmoucach <jpmoucachen@gmail.com>
2021-08-26 12:45:12 +0200

    changement mode draw

--- src/bodyDraw/humanDraw.cpp
+++ src/bodyDraw/humanDraw.cpp
@@ -23,10 +23,10 @@ void	drawLimb(Limb *limb, int &limbIndex, int selectedLimb, MatrixStack &matrixS
 	else
 		shader.setVector3f("color", 1.0f, 1.0f, 1.0f);
 	glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-	glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
+	glDrawArrays(GL_TRIANGLES, 0, 36);
 	shader.setVector3f("color", 0.0f, 0.0f, 0.0f);
 	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-	glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
+	glDrawArrays(GL_TRIANGLES, 0, 36);
 	while (iter != iterEnd)
 	{
 		limbIndex++;
