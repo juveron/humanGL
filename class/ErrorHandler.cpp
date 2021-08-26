d7ced2c4a844807aad15845861fbc58a83ab4634
jmoucach <jpmoucachen@gmail.com>
2021-08-26 12:44:37 +0200

    Ajout cas texture load

--- class/ErrorHandler.cpp
+++ class/ErrorHandler.cpp
@@ -4,21 +4,24 @@
 
 #include "../hdr/HumanGL.hpp"
 
-std::map<std::string, std::string> ErrorHandler::_errorDictionary {
+std::map<std::string, std::string> ErrorHandler::_errorDictionary{
 		{ "GLFW_INIT" , "Error with glfwInit" },
 		{ "WINDOW", "Error while creating window" },
 		{ "GLAD_LOAD", "Error with gladLoadGLLoader" },
 		{ "SHADER_CONSTRUCT", "Unable to open /read/close file for Shader"},
-		{ "SHADER_COMPILATION", "Error occurred during the compilation of the Shader" }
+		{ "SHADER_COMPILATION", "Error occurred during the compilation of the Shader" },
+		{ "TEXTURE_LOAD", "Couldn't load given texture" }
 };
 
-void ErrorHandler::setError(const std::string &errorKey) {
+void ErrorHandler::setError(const std::string &errorKey)
+{
 	glfwTerminate();
 	std::cout << ErrorHandler::_errorDictionary[errorKey] << std::endl;
 	exit(EXIT_FAILURE);
 }
 
-void ErrorHandler::setError(const std::string &errorKey, const char* errorMessage) {
+void ErrorHandler::setError(const std::string &errorKey, const char* errorMessage)
+{
 	glfwTerminate();
 	std::cout << ErrorHandler::_errorDictionary[errorKey] << ":\n" << errorMessage << std::flush;
 	exit(EXIT_FAILURE);
