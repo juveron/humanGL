b48173892df1d00b33058d0b9a405437d59f190c
jmoucach <jpmoucachen@gmail.com>
2021-08-26 12:45:29 +0200

    EBO retir e pour vbo complet

--- src/init/main.cpp
+++ src/init/main.cpp
@@ -24,41 +24,86 @@ int main(void)
 
 	Shader shader("./src/shader/vertexShader.glsl", "./src/shader/fragmentShader.glsl");
 
+	// -0.5f,  0.0f,  0.5f,  // A 0
+	//  0.5f,  0.0f,  0.5f,  // B 1
+	//  0.5f, -1.0f,  0.5f,  // C 2
+	// -0.5f, -1.0f,  0.5f,  // D 3
+	// -0.5f, -1.0f,  0.5f,  // D 3
+	// -0.5f,  0.0f, -0.5f,  // E 4
+	//  0.5f,  0.0f, -0.5f,  // F 5
+	//  0.5f, -1.0f, -0.5f,  // G 6
+	// -0.5f, -1.0f, -0.5f,  // H 7
+
+	// 	0, 2, 1, 0, 3, 2, // Front face
+	// 	4, 5, 6, 4, 6, 7, // Back face
+	// 	1, 2, 6, 1, 6, 5, // Right face
+	// 	0, 7, 3, 0, 4, 7, // Left face
+	// 	2, 3, 7, 2, 7, 6, // Bottom face
+	// 	0, 1, 5, 0, 5, 4  // Top face
 	float vertices[] = {
-		-0.5f,  0.0f,  0.5f,  // A 0
-		 0.5f,  0.0f,  0.5f,  // B 1
-		 0.5f, -1.0f,  0.5f,  // C 2
-		-0.5f, -1.0f,  0.5f,  // D 3
-		-0.5f,  0.0f, -0.5f,  // E 4
-		 0.5f,  0.0f, -0.5f,  // F 5
-		 0.5f, -1.0f, -0.5f,  // G 6
-		-0.5f, -1.0f, -0.5f,  // H 7
+		// FRONT FACE
+		-0.5f,  0.0f,  0.5f, 0.0f, 1.0f,
+		 0.5f, -1.0f,  0.5f, 1.0f, 0.0f,
+		 0.5f,  0.0f,  0.5f, 1.0f, 1.0f,
+		-0.5f,  0.0f,  0.5f, 0.0f, 1.0f,
+		-0.5f, -1.0f,  0.5f, 0.0f, 0.0f,
+		 0.5f, -1.0f,  0.5f, 1.0f, 0.0f,
+
+		 // BACK FACE
+		-0.5f,  0.0f, -0.5f, 1.0f, 1.0f,
+		 0.5f,  0.0f, -0.5f, 0.0f, 1.0f,
+		 0.5f, -1.0f, -0.5f, 0.0f, 0.0f,
+		-0.5f,  0.0f, -0.5f, 1.0f, 1.0f,
+		 0.5f, -1.0f, -0.5f, 0.0f, 0.0f,
+		-0.5f, -1.0f, -0.5f, 1.0f, 0.0f,
+
+		// RIGHT FACE
+		 0.5f,  0.0f,  0.5f, 0.0f, 1.0f,
+		 0.5f, -1.0f,  0.5f, 0.0f, 0.0f,
+		 0.5f, -1.0f, -0.5f, 1.0f, 0.0f,
+		 0.5f,  0.0f,  0.5f, 0.0f, 1.0f,
+		 0.5f, -1.0f, -0.5f, 1.0f, 0.0f,
+		 0.5f,  0.0f, -0.5f, 1.0f, 1.0f,
+
+		 // LEFT FACE
+		 -0.5f,  0.0f,  0.5f, 1.0f, 1.0f,
+		 -0.5f, -1.0f, -0.5f, 0.0f, 0.0f,
+		 -0.5f, -1.0f,  0.5f, 1.0f, 0.0f,
+		 -0.5f,  0.0f,  0.5f, 1.0f, 1.0f,
+		 -0.5f,  0.0f, -0.5f, 0.0f, 1.0f,
+		 -0.5f, -1.0f, -0.5f, 0.0f, 0.0f,
+
+		 // BOTTOM FACE
+		 0.5f, -1.0f,  0.5f, 1.0f, 1.0f, // C 2
+		-0.5f, -1.0f,  0.5f, 0.0f, 1.0f, // D 3
+		-0.5f, -1.0f, -0.5f, 0.0f, 0.0f, // H 7
+		 0.5f, -1.0f,  0.5f, 1.0f, 1.0f, // C 2
+		-0.5f, -1.0f, -0.5f, 0.0f, 0.0f, // H 7
+		 0.5f, -1.0f, -0.5f, 1.0f, 0.0f, // G 6
+
+		// TOP FACE
+		-0.5f,  0.0f,  0.5f, 0.0f, 0.0f,
+		 0.5f,  0.0f,  0.5f, 1.0f, 0.0f,
+		 0.5f,  0.0f, -0.5f, 1.0f, 1.0f,
+		-0.5f,  0.0f,  0.5f, 0.0f, 0.0f,
+		 0.5f,  0.0f, -0.5f, 1.0f, 1.0f,
+		-0.5f,  0.0f, -0.5f, 1.0f, 0.0f,
 	};
-	unsigned int indices[] = {
-		0, 2, 1, 0, 3, 2, // Front face
-		4, 5, 6, 4, 6, 7, // Back face
-		1, 2, 6, 1, 6, 5, // Right face
-		0, 7, 3, 0, 4, 7, // Left face
-		2, 3, 7, 2, 7, 6, // Bottom face
-		0, 1, 5, 0, 5, 4  // Top face
-	};
-	unsigned int VBO, VAO, EBO;
+	unsigned int VBO, VAO;
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
-	glGenBuffers(1, &EBO);
+
 	glBindVertexArray(VAO);
 
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	// position attribute
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
-
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
-	glBindVertexArray(0);
+	// texture coord attribute
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
 
 	glEnable(GL_CULL_FACE);
 	glEnable(GL_DEPTH_TEST);
@@ -75,6 +120,8 @@ int main(void)
 	shader.use();
 	shader.setMatrix("projMat", proj.matrix);
 	shader.setMatrix("viewMat", view.matrix);
+
+	unsigned int tex = generateTexture();
 
 	int current = 0;
 	s_body body[2];
@@ -97,6 +144,8 @@ int main(void)
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 		glBindVertexArray(VAO);
+		glBindTexture(GL_TEXTURE_2D, tex);
+
 
 		if (animationData.isAnimated) {
 			animationData.animationTime += deltaTime;
