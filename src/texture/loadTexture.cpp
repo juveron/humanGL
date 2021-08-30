ad83e07808db43830a41cfbb38e23aa99989257e
jmoucach <jpmoucachen@gmail.com>
2021-08-30 15:35:20 +0200

    updated texture loading

--- src/texture/loadTexture.cpp
+++ src/texture/loadTexture.cpp
@@ -3,25 +3,38 @@
 #define STB_IMAGE_IMPLEMENTATION
 #include "./stb_image.h"
 
-
-unsigned int generateTexture(void)
+static s_textureData loadTexture(const char *fileName)
 {
-	int width, height, nbrChannels;
-	unsigned int texture;
+	s_textureData textureData;
 
-	glGenTextures(1, &texture);
-	glBindTexture(GL_TEXTURE_2D, texture);
-
-	unsigned char *data = stbi_load("./src/texture/container.jpg", &width, &height, &nbrChannels, 0);
-
-	if (data) {
-		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
-		glGenerateMipmap(GL_TEXTURE_2D);
-	}
-	else {
+	textureData.data = stbi_load(fileName, &textureData.width,
+		&textureData.height, &textureData.nbrChannels, 0);
+	if (!textureData.data)
 		ErrorHandler::setError("TEXTURE_LOAD");
+	return textureData;
+}
+
+unsigned int *generateTextures(std::vector<const char *> texturePaths)
+{
+	unsigned int *textures = new unsigned int[texturePaths.size()];
+	size_t i = 0;
+	size_t texNum = texturePaths.size();
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+	glGenTextures(texturePaths.size(), textures);
+	std::vector<s_textureData> texturesData;
+
+	while (i < texNum)
+	{
+		s_textureData textureData = loadTexture(texturePaths[i]);
+		glBindTexture(GL_TEXTURE_2D, textures[i]);
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, textureData.width, textureData.height,
+			0, GL_RGB, GL_UNSIGNED_BYTE, textureData.data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+		stbi_image_free(textureData.data);
+		i++;
 	}
 
-	stbi_image_free(data);
-	return texture;
+	return textures;
 }
