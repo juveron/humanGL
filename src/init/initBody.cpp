cd4fe877f7aae47be12c550256e95ed4ac59bfbd
jmoucach <jpmoucachen@gmail.com>
2021-09-01 12:23:12 +0200

    initBody reference inutile retiree

--- src/init/initBody.cpp
+++ src/init/initBody.cpp
@@ -3,7 +3,7 @@
 //
 #include "HumanGL.hpp"
 
-void	initBody(s_body &body, int &currentIndex)
+void	initBody(s_body &body, int currentIndex)
 {
 
 	size_t i = 0;
@@ -17,7 +17,7 @@ void	initBody(s_body &body, int &currentIndex)
 	if (currentIndex == 0) positionHumanLimbs(body.limbs); else positionDoggoLimbs(body.limbs);
 };
 
-void	initLimb(s_body &body, int &currentIndex)
+void	initLimb(s_body &body, int currentIndex)
 {
 	body.limbs[body.selectedLimb]->translation = Vector3f(0, 0, 0);
 	body.limbs[body.selectedLimb]->scale = Vector3f(1.0f, 1.0f, 1.0f);
