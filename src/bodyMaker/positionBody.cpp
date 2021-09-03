08cfb9070a1f2f8284069cadc8240bfe111481f1
jmoucach <jpmoucachen@gmail.com>
2021-09-03 15:39:54 +0200

    Fct redondantes delete

--- src/bodyMaker/positionBody.cpp
+++
@@ -1,9 +0,0 @@
-#include "HumanGL.hpp"
-
-void positionBody(s_body &body, s_indexBody indexBody)
-{
-	if (indexBody.drawBody & 1)
-		positionHumanLimbs(body.limbs);
-	if ((indexBody.drawBody >> 1) & 1)
-		positionDoggoLimbs(body.limbs);
-}
