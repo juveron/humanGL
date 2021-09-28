dc68712ae4660e887ae746a8a9cc6025857be4ac
jmoucach <jpmoucachen@gmail.com>
2021-09-28 15:57:05 +0200

    Ajout classe font

---
+++ hdr/class/Font.hpp
@@ -0,0 +1,29 @@
+#ifndef FONT_HPP
+#define FONT_HPP
+
+#include "HumanGL.hpp"
+
+class Font
+{
+private:
+	std::map<GLchar, Character> _characters;
+	Shader _shader;
+	GLuint _vao, _vbo;
+
+	void loadCharacters(FT_Face const &face);
+
+public:
+	Font(std::string const &filePath, Shader const &c);
+	Font(Font const &c);
+	~Font(void);
+
+	std::map<GLchar, Character> getCharacters(void) const;
+	Shader getShader(void) const;
+	GLuint getVAO(void) const;
+	GLuint getVBO(void) const;
+
+	void renderText(std::string text, float x, float y, float scale, Vector3f color);
+	Font &operator=(Font const &rhs);
+};
+
+#endif
